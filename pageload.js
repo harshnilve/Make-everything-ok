57600bdd517dbab14169ffc5fd1b4976dd50f79f
harshnilve <136828096+harshnilve@users.noreply.github.com>
2023-06-19 15:57:14 +0530

    Add files via upload

---
+++ pageload.js
@@ -0,0 +1,11 @@
+function autoLoad() {
+    setTimeout(function(){ 
+      window.location.href = "https://www.example.com";
+    }, 5000);
+  }
+function autoLoad() {
+  setTimeout(function(){ 
+    window.location.href = "https://www.example.com";
+  }, 5000);
+}
+  
